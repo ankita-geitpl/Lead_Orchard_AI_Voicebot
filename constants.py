c183900326b8ebeb4bbcf8765774645bba7e76bc
akashhGE <akash.raut@goldeneagle.ai>
2024-03-29 15:19:44 +0530

    api key changes

--- constants.py
+++ constants.py
@@ -1,5 +1,5 @@
 # APIKEY ="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"
-APIKEY = "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"
+APIKEY = "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"
 # PostgreSQL Credentials
 db_params = {
         'host': 'localhost',
